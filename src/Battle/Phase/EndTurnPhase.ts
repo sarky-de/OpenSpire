58cdfb393a7751725f4f10f1e9b6806b7a4a1830
Jay Allan Jethwa <mail@jethwa.de>
2021-12-08 22:15:38 +0100

    Added battle phase stubs

---
+++ src/Battle/Phase/EndTurnPhase.ts
@@ -0,0 +1,15 @@
+import { GameState } from "../../Core/GameState";
+
+export class EndTurnPhase implements GameState {
+    Enter(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+    Exit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    Run(): void {
+        throw new Error("Method not implemented.");
+    }
+}
